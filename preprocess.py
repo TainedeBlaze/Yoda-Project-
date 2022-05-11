18f979db79603725d584129b90fb629f8954bf03
Julian Zille <julianzille@gmail.com>
2022-05-11 08:59:03 +0200

    'preprocessing'

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,13 @@
 from PIL import Image
 import numpy as np
+from numpy import savetxt
 
 image=Image.open('yoda.jpg')
-print(image.size)
-print(image.mode)
-image_np=np.asarray(image)
+print(image.size) # Eg 220x229
+print(image.mode) # RGB, grayscale
+image_np=np.asarray(image) 
 print("3D Array Size: ", image_np.size)
 img1d = np.reshape(image_np,(1,-1))
 print("1D Array Size: ", img1d.size)
+
+savetxt('1d.csv',img1d,delimiter=',')
