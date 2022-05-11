553ac2ffa20421d6aeff91be29bc5e948547bc4f
Julian Zille <julianzille@gmail.com>
2022-05-11 08:41:38 +0200

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,6 @@ image=Image.open('yoda.jpg')
 print(image.size)
 print(image.mode)
 image_np=np.asarray(image)
-
+print("3D Array Size: ", image_np.size)
 img1d = np.reshape(image_np,(1,-1))
-print(img1d.size)
+print("1D Array Size: ", img1d.size)
