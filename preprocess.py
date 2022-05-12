0c214215623709689ff1b4a5127107aea3758e99
beanzille <julianzille@gmail.com>
2022-05-12 16:53:15 +0200

    .

--- preprocess.py
+++ preprocess.py
@@ -5,15 +5,22 @@ from PIL import Image
 def readimage(imagename):
     print("Reading image " ,str(imagename)) 
     img = Image.open(filename) 
+    width , height = img.size
     pixelarray = np.array(img) 
-    width , height = img.size 
-    print("Image had width of " , width , " and height of " , height) 
+    pixelarray=pixelarray.reshape(-1)
+    # Adding dimensions to beginning of array
+    np.insert(pixelarray,0,height) 
+    np.insert(pixelarray,0,width)    
+    
+    print("Image had width of" , width , " and height of" , height) 
     
     #writing width, height and array to text file 
-    img1d = np.reshape(pixelarray,(1,-1)) 
-    outfile = imagename + ".txt" 
-    header = str(width) + ' , '+str(height) 
-    np.savetxt(outfile, img1d, fmt = '%d', delimiter = "," , header = header)   
+    img1d = np.reshape(pixelarray,-1)     
+
+    print(np.size(img1d))
+    outfile = imagename.split('.')[0] + ".txt" 
+    #header = str(width) + ' , '+str(height) 
+    np.savetxt(outfile, img1d, fmt = '%d', delimiter = ",")   
     print( "Output written to .txt file \n ") 
 
 #functionality allowing for multiple files to be read in 
